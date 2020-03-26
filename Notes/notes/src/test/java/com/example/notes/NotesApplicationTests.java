5c02a2e637d859b942ab359f3029b4f5547ff730
Darpan Patel <darpanpatel3131@gmail.com>
2020-03-25 22:09:52 -0300

    unit test added to find note

--- Notes/notes/src/test/java/com/example/notes/NotesApplicationTests.java
+++ Notes/notes/src/test/java/com/example/notes/NotesApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.notes;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class NotesApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void findNoteTest(){
+		String key = "moon";
+		try {
+			MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/findNote")
+					.param("key", "moon");
+			this.mockMvc.perform(builder).andExpect(MockMvcResultMatchers.status().isOk());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
